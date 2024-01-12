c3a57c1e6df14544e1c3d51ea75d4d8e347e2eec
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-01-13 02:47:12 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
 async function getGender(name) {
-    const base_api = "https://api.genderize.io";
-    const queryUrl = `${base_api}/?name=${name}&country_id=ID`;
-    const response = await fetch(queryUrl);
-    const artistDetail = await response.json();
-    return artistDetail;
+  const base_api = "https://api.genderize.io";
+  const queryUrl = `${base_api}/?name=${name}&country_id=ID`;
+  const response = await fetch(queryUrl);
+  const artistDetail = await response.json();
+  return artistDetail;
 }
 
-async function setGender (name) {
-    const data = await getGender(name);
-    const resultGender = document.getElementById("result-gender");
+async function setGender(name) {
+  const data = await getGender(name);
+  const resultGender = document.getElementById("result-gender");
 
-    const probability = data.probability * 100;
-    const gender = data.gender == "male" ? "Pria" : "Wanita";
+  const probability = data.probability * 100;
+  const gender = data.gender == "male" ? "Pria" : "Wanita";
 
-    resultGender.textContent = `Halo ${name}, presentase kemungkinan anda seorang ${gender} adalah sebanyak ${probability}%`;
+  resultGender.textContent = `Halo ${name}, presentase kemungkinan anda seorang ${gender} adalah sebanyak ${probability}%`;
 }
 
 async function handleInput(event) {
-    if (event.code == "Enter" || event.keyCode == 13) {
-        const query = event.target.value;
-        const name = query.replace(/\s/g, "");
-        await setGender(name);
-    }
+  if (event.code == "Enter" || event.keyCode == 13) {
+    const query = event.target.value;
+    const name = query.replace(/\s/g, "");
+    await setGender(name);
+  }
 }
